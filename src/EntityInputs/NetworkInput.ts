9ae3a4baadc0c062c4a643ff9bc4f2a1479676cd
michaelaboah <maboah@PlexusParc.com>
2022-11-20 11:51:31 -0500

    Added NetworkItem and NetworkInput to schema

---
+++ src/EntityInputs/NetworkInput.ts
@@ -0,0 +1,19 @@
+import { Field, InputType } from "type-graphql";
+import { NetworkType } from "../EntityAbstractions/Enums";
+import { NetworkPort } from "../EntityAbstractions/FieldObjects";
+import { IElectrical } from "../EntityAbstractions/IElectrical";
+
+@InputType()
+export default class NetworkInput {
+  @Field(() => NetworkType)
+  network_type!: NetworkType;
+
+  @Field(() => Boolean)
+  fiber?: boolean;
+
+  @Field(() => [NetworkPort])
+  network_connectivity?: NetworkPort[];
+
+  @Field(() => IElectrical)
+  power?: IElectrical;
+}
